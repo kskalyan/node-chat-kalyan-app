568ae8e2827a70b59fe0877dc77da7f013c1d052
SomaKalyana <ks.kalyan@gmail.com>
2018-09-09 21:54:58 -0400

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public')
 console.log(publicPath);
@@ -7,17 +9,29 @@ console.log(publicPath);
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     // res.send('Hello World!');
-//     res.render('home.hbs', {
-//         pageTitle: 'Home Page',
-//         welcomeMessage: 'Welcome to my Node Web Application'
-//     })
-// });
+io.on('connection', (socket) => {
+    console.log('New User connected')
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+
+    socket.emit('newMessage' , {
+        from: 'tripura@example.com',
+        text: 'Hi, this is tripura. How are you?',
+        createdAt: '1231'
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage);
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is up and running at port ${port}`);
 });
